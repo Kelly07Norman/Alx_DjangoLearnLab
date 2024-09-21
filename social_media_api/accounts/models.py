bad29ae032c5720b752c8abafb6ff9a3c6cc3d24
Kelly07Norman <82865397+Kelly07Norman@users.noreply.github.com>
2024-09-21 23:50:29 +0000

    Delete social_media_api directory

--- social_media_api/accounts/models.py
+++
@@ -1,7 +0,0 @@
-from django.contrib.auth.models import AbstractUser
-from django.db import models
-
-class CustomUser(AbstractUser):
-    bio = models.TextField(max_length=500, blank=True)
-    profile_picture = models.ImageField(upload_to='profiles/', blank=True, null=True)
-    followers = models.ManyToManyField('self', symmetrical=False, related_name='following')
