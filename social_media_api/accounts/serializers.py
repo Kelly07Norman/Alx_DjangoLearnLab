063d0efcef80aa1a48a9ac0e4fec8fc92cdea1ce
Kelly07Norman <82865397+Kelly07Norman@users.noreply.github.com>
2024-09-22 01:26:39 +0000

    Add files via upload

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -5,15 +5,13 @@ from rest_framework.authtoken.models import Token
 User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
-    # Ensure that the password is explicitly defined as a CharField
-    password = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
-    
+    password = serializers.CharField(write_only=True)
+
     class Meta:
         model = User
         fields = ['id', 'username', 'password', 'bio', 'profile_picture']
     
     def create(self, validated_data):
-        # Use get_user_model().objects.create_user for user creation
         user = get_user_model().objects.create_user(
             username=validated_data['username'],
             password=validated_data['password'],
