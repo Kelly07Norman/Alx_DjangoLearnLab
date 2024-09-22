ace0bd2c409e3ca726c1a5586add2599456c2cf0
Kelly07Norman <82865397+Kelly07Norman@users.noreply.github.com>
2024-09-22 01:19:36 +0000

    Add files via upload

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,29 +1,24 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
-from django.contrib.auth.password_validation import validate_password
 from rest_framework.authtoken.models import Token
 
 User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
-    username = serializers.CharField(required=True)
-    email = serializers.EmailField(required=True)
-    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
-    password2 = serializers.CharField(write_only=True, required=True)
-    token = serializers.CharField(read_only=True)
-
+    # Ensure that the password is explicitly defined as a CharField
+    password = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
+    
     class Meta:
         model = User
-        fields = ('id', 'username', 'email', 'password', 'password2', 'bio', 'profile_picture', 'token')
-
-    def validate(self, attrs):
-        if attrs['password'] != attrs['password2']:
-            raise serializers.ValidationError({"password": "Password fields didn't match."})
-        return attrs
-
+        fields = ['id', 'username', 'password', 'bio', 'profile_picture']
+    
     def create(self, validated_data):
-        validated_data.pop('password2')
-        user = User.objects.create_user(**validated_data)
-        token = Token.objects.create(user=user)
-        user.token = token.key
+        # Use get_user_model().objects.create_user for user creation
+        user = get_user_model().objects.create_user(
+            username=validated_data['username'],
+            password=validated_data['password'],
+            bio=validated_data.get('bio', ''),
+            profile_picture=validated_data.get('profile_picture', None)
+        )
+        Token.objects.create(user=user)
         return user
