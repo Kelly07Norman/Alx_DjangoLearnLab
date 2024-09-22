49b9c6bdcc3213393bfebded98482ae84722df7f
Kelly07Norman <82865397+Kelly07Norman@users.noreply.github.com>
2024-09-22 00:50:26 +0000

    update

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,14 +1,25 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
 
 User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    token = serializers.CharField(read_only=True)
+
     class Meta:
         model = User
-        fields = ('id', 'username', 'email', 'password', 'bio', 'profile_picture')
-        extra_kwargs = {'password': {'write_only': True}}
+        fields = ('id', 'username', 'email', 'password', 'bio', 'profile_picture', 'token')
 
     def create(self, validated_data):
-        user = User.objects.create_user(**validated_data)
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password'],
+            bio=validated_data.get('bio', ''),
+            profile_picture=validated_data.get('profile_picture', None)
+        )
+        Token.objects.create(user=user)
+        user.token = user.auth_token.key
         return user
--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, status
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
@@ -7,6 +7,16 @@ from .serializers import UserSerializer
 class RegisterView(generics.CreateAPIView):
     serializer_class = UserSerializer
     permission_classes = (permissions.AllowAny,)
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        headers = self.get_success_headers(serializer.data)
+        return Response({
+            'token': user.token,
+            'user': UserSerializer(user, context=self.get_serializer_context()).data
+        }, status=status.HTTP_201_CREATED, headers=headers)
 
 class LoginView(ObtainAuthToken):
     def post(self, request, *args, **kwargs):
